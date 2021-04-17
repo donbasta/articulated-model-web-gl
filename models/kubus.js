abf08bda1a2ba68f19b2b6eceb3778727973609d
donbasta <faddilafarras@gmail.com>
2021-04-17 10:41:36 +0700

    add kubus

---
+++ models/kubus.js
@@ -0,0 +1,64 @@
+
+
+const baseVal = 100;
+export const kubus = {
+    positions: [
+        0,0,0,
+        baseVal,0,0,
+        0,baseVal,0,
+        baseVal,0,0,
+        0,baseVal,0,
+        baseVal,baseVal,0,
+
+        0,0,baseVal,
+        baseVal,0,baseVal,
+        0,baseVal,baseVal,
+        baseVal,0,baseVal,
+        0,baseVal,baseVal,
+        baseVal,baseVal,baseVal,
+
+        0,0,0,
+        0,baseVal,0,
+        0,0,baseVal,
+        0,baseVal,0,
+        0,0,baseVal,
+        0,baseVal,baseVal,
+
+        baseVal,0,0,
+        baseVal,baseVal,0,
+        baseVal,0,baseVal,
+        baseVal,baseVal,0,
+        baseVal,0,baseVal,
+        baseVal,baseVal,baseVal,
+        
+        0,0,0,
+        baseVal,0,0,
+        0,0,baseVal,
+        baseVal,0,0,
+        0,0,baseVal,
+        baseVal,0,baseVal,
+
+        0,baseVal,0,
+        baseVal,baseVal,0,
+        0,baseVal,baseVal,
+        baseVal,baseVal,0,
+        0,baseVal,baseVal,
+        baseVal,baseVal,baseVal
+    ],
+    colors: [
+        1.0, 0, 0, 1.0,
+        1.0, 0, 0, 1.0,
+        0, 0, 1.0, 1.0,
+        0, 0, 1.0, 1.0,
+        0, 1.0, 0, 1.0,
+        0, 1.0, 0, 1.0,
+        1.0, 0, 1.0, 1.0,
+        1.0, 0, 1.0, 1.0,
+        0, 1.0, 1.0, 1.0,
+        0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 0, 1.0,
+        1.0, 1.0, 0, 1.0,
+    ]
+}
+
+// export default kubus;
